451cb2f3d8d1c96e6a357bf0ef6067ab4ddc1e00
ben <ben.harder2@gmail.com>
2018-08-30 10:05:55 -0700

    working on last page

--- interactivity.js
+++ interactivity.js
@@ -1,18 +1,19 @@
+$(document).on('pagebeforeshow', '#index', function() {
+    $("#tabs").tabs();
+});
+
 $('#aboutTab').click(function (e)
 {
     e.preventDefault()
     $(this).tab('show')
 });
+
 $('#workExperienceTab').click(function (e)
 {
     e.preventDefault()
     $(this).tab('show')
 });
-$('#schoolTab').click(function (e)
-{
-    e.preventDefault()
-    $(this).tab('show')
-});
+
 $('#projectsTab').click(function (e)
 {
     e.preventDefault()
