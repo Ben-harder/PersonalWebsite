1a81ff4a5ace7cef1e93db765b2802f585c032f8
Ben Harder <ben.harder2@gmail.com>
2020-01-25 21:32:30 -0800

    Fixing n-body

--- JavaScript/n-body.js
+++ JavaScript/n-body.js
@@ -6,7 +6,7 @@ var refresh = setInterval(frame, 20);
 var canvas = document.getElementById("nBodyCanvas");
 var nBodyctx = canvas.getContext('2d');
 var particles = [];
-var gravitationalConstant = 6.67408*Math.pow(10, -2);
+var gravitationalConstant = 6.67408 * Math.pow(10, -2);
 
 
 var pp = new Particle(15, 150, 300);
@@ -18,31 +18,26 @@ particles.push(gg);
 
 
 
-function frame()
-{
-    draw(ctx);
+function frame() {
+    draw(nBodyctx);
 }
 
 /*
  * This function will draw all the particles in their current state.
  */
-function draw(ctx)
-{
-   nBodyctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-   nBodyctx.fillStyle = "#FFFFFF";
+function draw(ctx) {
+    nBodyctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+    nBodyctx.fillStyle = "#FFFFFF";
 
     // Update the position and move all the particles.
-    for (var i = 0; i < particles.length; i++)
-    {
+    for (var i = 0; i < particles.length; i++) {
         var currParticle = particles[i];
         // Check to see if the particle left the canvas
-        if (currParticle.pos.first > canvas.width || currParticle.pos.second > canvas.height || currParticle.pos.first < 0 || currParticle.pos.second < 0)
-        {
+        if (currParticle.pos.first > canvas.width || currParticle.pos.second > canvas.height || currParticle.pos.first < 0 || currParticle.pos.second < 0) {
             delete currParticle;
             particles.splice(i, 1);
         }
-        else
-        {
+        else {
             // Check to see if the particle should be merged
             mergeAllClose(currParticle);
 
@@ -59,11 +54,11 @@ function draw(ctx)
             var radius = Math.sqrt(currParticle.mass / Math.PI) * 2;
 
             // Draw the particle
-           nBodyctx.beginPath();
+            nBodyctx.beginPath();
 
-           nBodyctx.ellipse(currParticle.pos.first, currParticle.pos.second, radius, radius, 0, 0, 2 * Math.PI);
+            nBodyctx.ellipse(currParticle.pos.first, currParticle.pos.second, radius, radius, 0, 0, 2 * Math.PI);
 
-           nBodyctx.fill();
+            nBodyctx.fill();
         }
     }
 
@@ -77,18 +72,15 @@ function draw(ctx)
  * 
  * Formula found here: https://en.wikipedia.org/wiki/N-body_problem
  */
-function calculateForce(particle)
-{
+function calculateForce(particle) {
     var netForceX = 0;
     var netForceY = 0;
 
-    for (var i = 0; i < particles.length; i++)
-    {
+    for (var i = 0; i < particles.length; i++) {
         var currParticle = particles[i];
 
         // Account for the particle not being the passed particle
-        if (!currParticle.equals(particle))
-        {
+        if (!currParticle.equals(particle)) {
             var massProduct = currParticle.mass * particle.mass;
             var netParticleMag = Math.sqrt(Math.pow(currParticle.pos.first - particle.pos.first, 2) + Math.pow(currParticle.pos.second - particle.pos.second, 2));
             var netMagCubed = Math.pow(netParticleMag, 3);
@@ -106,19 +98,23 @@ function calculateForce(particle)
  */
 var startPos;
 
-canvas.addEventListener("mousedown", function ()
-{
+canvas.addEventListener("mousedown", function () {
     //console.log("Mouse down at " + (event.pageX - canvas.offsetLeft) + ", " + (event.pageY - canvas.offsetTop));
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
 
-    startPos = new Pair(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+    startPos = new Pair(x, y);
 }, false);
 
-canvas.addEventListener("mouseup", function ()
-{
-    //console.log("Mouse up at " + (event.pageX - canvas.offsetLeft) + ", " + (event.pageY - canvas.offsetTop));
+canvas.addEventListener("mouseup", function () {
+    
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
 
     // Mass equals the vector magnitude between mouse down and mouse up
-    var endPos = new Pair(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+    var endPos = new Pair(x, y);
     var dx = Math.abs(endPos.first - startPos.first);
     var dy = Math.abs(endPos.second - startPos.second);
     var mass = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
@@ -134,21 +130,17 @@ canvas.addEventListener("mouseup", function ()
 /*
  * This function will loop through the particles and see if there are particles that meet a distance threshold to merge them.
  */
-function mergeAllClose(particle)
-{
+function mergeAllClose(particle) {
     var joinThreshhold = Math.sqrt(particle.mass / Math.PI) * 2;
 
-    for (var i = 0; i < particles.length; i++)
-    {
+    for (var i = 0; i < particles.length; i++) {
         var currParticle = particles[i];
 
         // Make sure it's not the same particle
-        if (!currParticle.equals(particle))
-        {
+        if (!currParticle.equals(particle)) {
             var netParticleMag = Math.sqrt(Math.pow(particle.pos.first - currParticle.pos.first, 2) + Math.pow(particle.pos.second - currParticle.pos.second, 2));
 
-            if (netParticleMag <= joinThreshhold)
-            {
+            if (netParticleMag <= joinThreshhold) {
                 particle.merge(currParticle);
                 delete currParticle;
                 particles.splice(i, 1);
@@ -160,10 +152,8 @@ function mergeAllClose(particle)
 /* 
  * This function will clear all the particles from the canvas.
  */
-function clearParticles()
-{
-    for (var i = particles.length; i >= 0; i--)
-    {
+function clearParticles() {
+    for (var i = particles.length; i >= 0; i--) {
         delete particles[i];
         particles.splice(i, 1);
     }
@@ -171,7 +161,6 @@ function clearParticles()
 
 var clearParticlesButton = document.getElementById("clearParticlesButton");
 
-clearParticlesButton.addEventListener("click", function () 
-{
+clearParticlesButton.addEventListener("click", function () {
     clearParticles();
 }, false);
